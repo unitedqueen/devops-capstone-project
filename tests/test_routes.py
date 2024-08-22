8d73e217690edb38f41f479ab248d6a588d9b089
unitedqueen <emaediongsabbath@gmail.com>
2024-08-22 18:16:59 -0400

    nose arguments

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -16,8 +16,16 @@ from service.routes import app
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
 
-BASE_URL = "/accounts"
 
 
 ######################################################################
